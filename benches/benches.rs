074422cedb3294bd3ca5f7ac69c2db6754109048
Alexander Gryaznov <hi@agryaznov.com>
2022-10-11 20:40:19 +0300

    added bench for an instrumented wasm-coremark

--- benches/benches.rs
+++ benches/benches.rs
@@ -50,5 +50,61 @@ fn stack_height_limiter(c: &mut Criterion) {
 	});
 }
 
-criterion_group!(benches, gas_metering, stack_height_limiter);
+fn gas_metered_coremark(c: &mut Criterion) {
+	use std::time::{SystemTime, UNIX_EPOCH};
+	use wasmi::*;
+	use wasmi_core::F32;
+
+	let mut group = c.benchmark_group("Coremark: metered with ImportedFunctionInjector");
+	let wasm_filename = "coremark_minimal.wasm";
+	let bytes = read(fixture_dir().join(wasm_filename)).unwrap();
+	group.throughput(Throughput::Bytes(bytes.len().try_into().unwrap()));
+
+	group.bench_with_input(wasm_filename, &bytes, |bench, input| {
+		bench.iter(|| {
+			let module = deserialize_buffer(input).unwrap();
+			let instrumented_module =
+				ImportedFunctionInjector("env").inject(&module, &ConstantCostRules::default());
+
+			// wasmi magic
+			let engine = Engine::default();
+			let module = wasmi::Module::new(&engine, &mut &input[..]).unwrap();
+
+			// host stores gas_left as `u64`
+			type HostState = u64;
+			// we init host state with maximum gas_left
+			let mut store = Store::new(&engine, u64::MAX);
+			// create gas host function
+			let host_gas =
+				Func::wrap(&mut store, |mut caller: Caller<'_, HostState>, param: u64| {
+					*caller.host_data_mut() -= param;
+				});
+
+			// create clock_ms host function
+			let host_clock_ms = Func::wrap(&mut store, || {
+				SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64
+			});
+
+			// link the host functions with the imported ones
+			let mut linker = <Linker<HostState>>::new();
+			linker.define("env", "gas", host_gas).unwrap();
+			linker.define("env", "clock_ms", host_clock_ms).unwrap();
+			let instance =
+				linker.instantiate(&mut store, &module).unwrap().start(&mut store).unwrap();
+
+			let run = instance
+				.get_export(&store, "run")
+				.and_then(Extern::into_func)
+				.unwrap()
+				.typed::<(), F32, _>(&mut store)
+				.unwrap();
+
+			// And finally we can call the wasm!
+			run.call(&mut store, ()).unwrap();
+		})
+	});
+}
+
+//criterion_group!(benches, gas_metering, stack_height_limiter);
+criterion_group!(benches, gas_metered_coremark);
 criterion_main!(benches);
