ad12c850c574c349e37d59bd25cc9aaee018ede0
Alexander Gryaznov <hi@agryaznov.com>
2022-10-19 21:44:43 +0300

    save before re-factoring prepare_in_wasm()

--- benches/benches.rs
+++ benches/benches.rs
@@ -72,18 +72,20 @@ impl Prepare for mutable_global::Injector {
 }
 
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use wasmi::*;
-fn prepare_in_wasmi<P: Prepare + Backend>(
-	backend: P,
-	input: &[u8],
-) -> (TypedFunc<(), core::F32>, Store<u64>) {
+use wasmi::{
+	self,
+	core::{Value, ValueType},
+	Caller, Config, Engine, Extern, Func, Instance, Linker, StackLimits, Store,
+};
+
+fn prepare_in_wasmi<P: Prepare + Backend>(backend: P, input: &[u8]) -> (Instance, Store<u64>) {
 	let module = deserialize_buffer(input).unwrap();
 	let gas_preps = backend.gas_preps();
 	let instrumented_module =
 		gas_metering::inject(module, backend, &ConstantCostRules::default()).unwrap();
 	let input = serialize(instrumented_module).unwrap();
 	// Prepare wasmi
-	let engine = Engine::default();
+	let engine = Engine::new(&bench_config());
 	let module = wasmi::Module::new(&engine, &mut &input[..]).unwrap();
 	// Host stores gas_left as `u64`
 	type HostState = u64;
@@ -93,14 +95,15 @@ fn prepare_in_wasmi<P: Prepare + Backend>(
 	let host_gas = Func::wrap(&mut store, |mut caller: Caller<'_, HostState>, param: u64| {
 		*caller.host_data_mut() -= param;
 	});
-	// Create clock_ms host function
-	let host_clock_ms = Func::wrap(&mut store, || {
-		SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64
-	});
-	// Link the host functions with the imported ones
+	// Create clock_ms host function.
+	// TBD: make it if needed: for coremark run
+	// let host_clock_ms = Func::wrap(&mut store, || {
+	// 	SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64
+	// });
+	// // Link the host functions with the imported ones
 	let mut linker = <Linker<HostState>>::new();
-	// Clock for time measuring from the coremark wasm
-	linker.define("env", "clock_ms", host_clock_ms).unwrap();
+	// // Clock for time measuring from the coremark wasm
+	// linker.define("env", "clock_ms", host_clock_ms).unwrap();
 	// Link host gas function if needed
 	if gas_preps {
 		linker.define("env", "gas", host_gas).unwrap();
@@ -113,18 +116,20 @@ fn prepare_in_wasmi<P: Prepare + Backend>(
 			.get_export(&mut store, "gas_left")
 			.and_then(Extern::into_global)
 			.unwrap()
-			.set(&mut store, core::Value::I64(i64::MAX))
+			.set(&mut store, Value::I64(i64::MAX))
 			.unwrap();
 	};
 
-	let run = instance
-		.get_export(&mut store, "run")
-		.and_then(Extern::into_func)
-		.unwrap()
-		.typed::<(), core::F32>(&mut store)
-		.unwrap();
+	// TBD: make it if needed: for coremark run
+	// TBD: convertion to TypedFunc<(), F32> from instance outside this func
+	// let run = instance
+	// 	.get_export(&mut store, "run")
+	// 	.and_then(Extern::into_func)
+	// 	.unwrap()
+	// 	.typed::<(), F32>(&mut store)
+	// 	.unwrap();
 
-	(run, store)
+	(instance, store)
 }
 
 fn gas_metered_coremark(c: &mut Criterion) {
@@ -152,14 +157,85 @@ fn gas_metered_coremark(c: &mut Criterion) {
 	});
 }
 
+/// Converts the `.wat` encoded `bytes` into `.wasm` encoded bytes.
+pub fn wat2wasm(bytes: &[u8]) -> Vec<u8> {
+	wat::parse_bytes(bytes).unwrap().into_owned()
+}
+
+/// Returns a [`Config`] useful for benchmarking.
+fn bench_config() -> Config {
+	let mut config = Config::default();
+	config.set_stack_limits(StackLimits::new(1024, 1024 * 1024, 64 * 1024).unwrap());
+	config
+}
+
+fn wasmi_execute_bare_call_16(c: &mut Criterion) {
+	let mut group = c.benchmark_group("bare_call, instrumented");
+	const REPETITIONS: usize = 20_000;
+	let wasm_bytes = wat2wasm(include_bytes!("fixtures/wat/bare_call.wat"));
+	let params = &[
+		Value::default(ValueType::I32),
+		Value::default(ValueType::I64),
+		Value::default(ValueType::F32),
+		Value::default(ValueType::F64),
+		Value::default(ValueType::I32),
+		Value::default(ValueType::I64),
+		Value::default(ValueType::F32),
+		Value::default(ValueType::F64),
+		Value::default(ValueType::I32),
+		Value::default(ValueType::I64),
+		Value::default(ValueType::F32),
+		Value::default(ValueType::F64),
+		Value::default(ValueType::I32),
+		Value::default(ValueType::I64),
+		Value::default(ValueType::F32),
+		Value::default(ValueType::F64),
+	];
+	let results = &mut [Value::I32(0); 16];
+
+	group.bench_function("with host_function::Injector", |bench| {
+		let backend = host_function::Injector::new("env", "gas");
+		let (instance, mut store) = prepare_in_wasmi(backend, &wasm_bytes);
+		let bare_call =
+			instance.get_export(&store, "bare_call_16").and_then(Extern::into_func).unwrap();
+		bench.iter(|| {
+			for _ in 0..REPETITIONS {
+				bare_call.call(&mut store, params, results).unwrap();
+			}
+		})
+	});
+
+	group.bench_function("with mutable_global::Injector", |bench| {
+		let backend = mutable_global::Injector::new("gas_left");
+		let (instance, mut store) = prepare_in_wasmi(backend, &wasm_bytes);
+		let bare_call =
+			instance.get_export(&store, "bare_call_16").and_then(Extern::into_func).unwrap();
+		bench.iter(|| {
+			for _ in 0..REPETITIONS {
+				bare_call.call(&mut store, params, results).unwrap();
+			}
+		})
+	});
+}
+
 criterion_group!(benches, gas_metering, stack_height_limiter);
+// criterion_group!(
+// 	name = contest_backends;
+// 	config = Criterion::default()
+// 		.sample_size(10)
+// 		.measurement_time(Duration::from_millis(250000))
+// 		.warm_up_time(Duration::from_millis(1000));
+// 	targets =
+// 		 gas_metered_coremark,
+// );
 criterion_group!(
-	name = contest_backends;
+	name = wasmi_execute;
 	config = Criterion::default()
 		.sample_size(10)
 		.measurement_time(Duration::from_millis(250000))
-		.warm_up_time(Duration::from_millis(1000));
+	.warm_up_time(Duration::from_millis(1000));
 	targets =
-		 gas_metered_coremark,
+		 wasmi_execute_bare_call_16,
 );
-criterion_main!(benches, contest_backends);
+//criterion_main!(benches, contest_backends);
+criterion_main!(wasmi_execute);
