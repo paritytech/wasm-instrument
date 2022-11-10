d887e72e0f9d22a2acdde178e8bde1e2404e4819
Alexander Gryaznov <hi@agryaznov.com>
2022-11-10 18:43:59 +0200

    1033% overhead on many_blocks.wasm when mut_global gas_metering together with stack_height

--- tests/overhead.rs
+++ tests/overhead.rs
@@ -3,7 +3,7 @@ use std::{
 	path::PathBuf,
 };
 use wasm_instrument::{
-	gas_metering::{self, host_function, ConstantCostRules},
+	gas_metering::{self, host_function, mutable_global, ConstantCostRules},
 	inject_stack_limiter,
 	parity_wasm::{deserialize_buffer, elements::Module, serialize},
 };
@@ -16,12 +16,7 @@ fn fixture_dir() -> PathBuf {
 	path
 }
 
-/// Print the overhead of applying gas metering with host_function::Injector, stack
-/// height limiting or both.
-///
-/// Use `cargo test print_overhead -- --nocapture`.
-#[test]
-fn print_size_overhead() {
+fn overheads() {
 	let mut results: Vec<_> = read_dir(fixture_dir())
 		.unwrap()
 		.map(|entry| {
@@ -32,11 +27,21 @@ fn print_size_overhead() {
 				let module: Module = deserialize_buffer(&bytes).unwrap();
 				(len, module)
 			};
-			let (gas_metering_len, gas_module) = {
-				let backend = host_function::Injector::new("env", "gas");
+			let (gas_metered_host_fn_len, gas_metered_host_fn_module) = {
 				let module = gas_metering::inject(
 					orig_module.clone(),
-					backend,
+					host_function::Injector::new("env", "gas"),
+					&ConstantCostRules::default(),
+				)
+				.unwrap();
+				let bytes = serialize(module.clone()).unwrap();
+				let len = bytes.len();
+				(len, module)
+			};
+			let (gas_metered_mut_global_len, gas_metered_mut_global_module) = {
+				let module = gas_metering::inject(
+					orig_module.clone(),
+					mutable_global::Injector::new("gas_left"),
 					&ConstantCostRules::default(),
 				)
 				.unwrap();
@@ -49,23 +54,34 @@ fn print_size_overhead() {
 				let bytes = serialize(module).unwrap();
 				bytes.len()
 			};
-			let both_len = {
-				let module = inject_stack_limiter(gas_module, 128).unwrap();
+			let gas_metered_host_fn_both_len = {
+				let module = inject_stack_limiter(gas_metered_host_fn_module, 128).unwrap();
 				let bytes = serialize(module).unwrap();
 				bytes.len()
 			};
 
-			let overhead = both_len * 100 / orig_len;
+			let gas_metered_mut_global_both_len = {
+				let module = inject_stack_limiter(gas_metered_mut_global_module, 128).unwrap();
+				let bytes = serialize(module).unwrap();
+				bytes.len()
+			};
+
+			let overhead_host_fn = gas_metered_host_fn_both_len * 100 / orig_len;
+			let overhead_mut_global = gas_metered_mut_global_both_len * 100 / orig_len;
 
 			(
-				overhead,
+			    overhead_mut_global,
 				format!(
-					"{:30}: orig = {:4} kb, gas_metering = {} %, stack_limiter = {} %, both = {} %",
+					"{:30}: orig = {:4} kb, stack_limiter = {} %, gas_metered_host_fn =    {} %, both = {} %,\n \
+					 {:69} gas_metered_mut_global = {} %, both = {} %",
 					entry.file_name().to_str().unwrap(),
 					orig_len / 1024,
-					gas_metering_len * 100 / orig_len,
 					stack_height_len * 100 / orig_len,
-					overhead,
+					gas_metered_host_fn_len * 100 / orig_len,
+				    overhead_host_fn,
+				    "",
+					gas_metered_mut_global_len * 100 / orig_len,
+					overhead_mut_global,
 				),
 			)
 		})
@@ -74,4 +90,19 @@ fn print_size_overhead() {
 	for entry in results {
 		println!("{}", entry.1);
 	}
+}
+
+/// Print the overhead of applying gas metering with host_function::Injector, stack
+/// height limiting or both.
+///
+/// Use `cargo test print_size_overhead -- --nocapture`.
+#[test]
+fn print_size_overhead() {
+	//    let mut_global_backend = mutable_global::Injector::new("gas_left");
+
+	overheads();
+	// overheads_for_backend::<wasm_instrument::gas_metering::host_function::Injector,
+	// Clone>(host_fn_backend);
+	// overheads_for_backend::<wasm_instrument::gas_metering::mutable_global::Injector,
+	// Clone>(mut_global_backend);
 }
